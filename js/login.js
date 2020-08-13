fca76f1f1655bf78e68d605a69c84efb625c8b88
frhez <frhezury@hotmail.com>
2020-08-13 16:25:14 -0300

    Añadido boton para salir de la sesion en la pantalla principal

--- js/login.js
+++ js/login.js
@@ -1,6 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+function onSignIn() {
+    location.href = "cover.html";
+}
+
+function onSignOut() {
+    var auth2;
+    var signedIn;
+    try {
+        signedIn = auth2.isSignedIn.get();
+        nonExistentFunction();
+        if (signedIn) {
+            gapi.auth2.GetAuthInstance().signOut().then(function () {
+                location.href = "index.html";
+            });
+        }
+    } catch (error) {
+        location.href = "index.html";
+    }
+}
