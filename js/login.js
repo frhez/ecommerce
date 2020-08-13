563cc08e470be9198c91c876612db79a5f61f2d6
frhez <frhezury@hotmail.com>
2020-08-13 18:07:42 -0300

    Quitado el botón de salir

--- js/login.js
+++ js/login.js
@@ -2,36 +2,8 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    //Loads the API
-    gapi.load('auth2', function () {
-
-        gapi.auth2.init({
-
-            client_id: "490154075016-33kn7mda6khg18l899gn8m0bpcnbomdg.apps.googleusercontent.com",
-
-        })
-    });
 });
 
 function onSignIn() {
     location.href = "cover.html";
-}
-
-function onSignOut() {
-    var signedIn;
-    try {
-        signedIn = auth2.isSignedIn.get();
-        if (signedIn) {
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-                auth2.disconnect();
-            });
-            location.href = "index.html";
-        }
-        else{
-            location.href = "index.html";
-        }
-    } catch (error) {
-        console.log(error);
-    }
 }
