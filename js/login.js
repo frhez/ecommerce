12ed18682d5a944306e362c853bd7c39e124d95f
frhez <frhezury@hotmail.com>
2020-08-15 17:17:33 -0300

    Login funcional

--- js/login.js
+++ js/login.js
@@ -4,6 +4,76 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 });
 
+function enterSite() {
+    if (checkUserInfo()) {
+        location.href = "cover.html";
+    }
+}
+
+function checkUserInfo() {
+    let userField = document.getElementById("userField").value;
+    let passwordField = document.getElementById("passwordField").value;
+    let userInfo = document.getElementById("userFieldInfo");
+    let passwordInfo = document.getElementById("passwordFieldInfo");
+
+    let userOk = false;
+    let passOk = false; 
+    //User info
+    if (userField.length > userField.trim().length) { //Check for empty fields
+        userInfo.innerHTML = "No puede ingresar espacios en blanco!";
+    }
+    else if (userField.trim() === "") {
+        userInfo.innerHTML = "El campo no puede quedar vacío!";
+    }
+    else{
+        userInfo.innerHTML = "";
+        userOk = true;
+    }
+
+    //Password info
+    if (passwordField.length > passwordField.trim().length) { //Check for empty fields
+        passwordInfo.innerHTML = "No puede ingresar espacios en blanco!";
+        return false;
+    }
+    else if (passwordField.trim() === "") {
+        passwordInfo.innerHTML = "El campo no puede quedar vacío!";
+        return false;
+    }
+    else {
+        passwordInfo.innerHTML = "";
+        passOk = true;
+    }
+
+    return userOk && passOk;
+    /*
+    if (userField.trim() === "" || passwordField.trim() === "") { //Check for empty fields
+        alert("Hay campos sin completar");
+        return false;
+    }
+    else if (userField.length > userField.trim().length || passwordField.length > passwordField.trim().length) {
+        alert("No puede ingresar espacios en blanco!");
+        return false;
+    }
+    else {
+        return true;
+    }
+    */
+}
+
 function onSignIn() {
     location.href = "cover.html";
+}
+
+//Sign out the user
+function onSignOut() {
+    try {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            location.href = "index.html";
+        });
+    }
+    catch (error) {
+        console.error(error);
+        location.href = "index.html";
+    }
 }
